d61650a338087eae1691a3bdd476064e96a4f5de
Denis <dmarkitanov@gmail.com>
2019-10-05 13:38:58 +0600

    added validation for login

--- src/app/auth/logreg/logreg.component.ts
+++ src/app/auth/logreg/logreg.component.ts
@@ -12,14 +12,21 @@ export class LogregComponent implements OnInit {
   ngOnInit() {
   }
 
+  loginSubmition(emailLog, passwordLog) {
+    if (!emailLog.value || !passwordLog.value) return false
+
+    alert('Logged In!')
+    return true
+  }
+
   registerSubmition(emailReg, passwordReg, confirmPassword) {
-    if(!emailReg.value.includes('@')) return false;
+    if(!emailReg.value.includes('@')) return false
     if (!emailReg.value || !passwordReg.value) return false
     if (passwordReg.value <= 6) return false
     if (passwordReg.value != confirmPassword.value) return false
 
     alert('Registered!')
-    return true;
+    return true
   }
 
 }
