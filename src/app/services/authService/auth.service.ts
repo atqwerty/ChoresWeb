adb62a9ea61cb064c2c57956ac2a530f948ea116
Denis <dmarkitanov@gmail.com>
2019-10-26 21:59:59 +0600

    auth service done

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/classes/user/user'
+import { HttpClient } from 'selenium-webdriver/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private currentUserSubject: BehaviorSubject<User>
+  public currentUser: Observable<User>
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')))
+    this.currentUser = this.currentUserSubject.asObservable()
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+      .pipe(map(user => {
+        localStorage.setItem('currentUser', JSON.stringify(user))
+        this.currentUserSubject.next(user)
+        return user
+      }))
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser')
+    this.currentUserSubject.next(null)
+  }
 }
