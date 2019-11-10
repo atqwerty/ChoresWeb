9a96887f62f8db11e88305a9aaa38b01d418e67f
Denis <dmarkitanov@gmail.com>
2019-11-10 11:14:05 +0600

    animation works, need to fix styles

--- src/app/shared/route-animation.ts
+++ src/app/shared/route-animation.ts
@@ -10,35 +10,35 @@ import {
  export const slideInAnimation =
     trigger('routeAnimations', [
          transition('Board => *', [
-              query(':enter, :leave', 
+              query(':enter, :leave',
                    style({ position: 'fixed', width: '100%' }), 
                    { optional: true }),        
               group([
                    query(':enter',[
-                       style({ transform: 'translateX(-100%)' }),
-                       animate('0.5s ease-in-out', 
+                       style({ transform: 'translateX(100%)' }),
+                       animate('1s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                        style({ transform:   'translateX(0%)'}),
-                       animate('0.5s ease-in-out', 
+                       animate('1s ease-in-out', 
                        style({ transform: 'translateX(100%)' }))
                    ], { optional: true }),
               ])
          ]),
          transition('Home => *', [
               query(':enter, :leave', 
-                   style({ position: 'fixed',  width: '100%' }), 
+                   style({ position: 'fixed',  width: '100%', background: 'white' }), 
                    { optional: true }),
               group([
                    query(':enter', [
                        style({ transform: 'translateX(100%)' }), 
-                       animate('0.5s ease-in-out', 
+                       animate('1s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                        style({ transform: 'translateX(0%)' }),
-                       animate('0.5s ease-in-out', 
+                       animate('1s ease-in-out', 
                        style({ transform: 'translateX(-100%)' }))
                        ], { optional: true }),
                ])
@@ -50,12 +50,12 @@ import {
                group([
                    query(':enter', [
                        style({ transform: 'translateX(100%)' }),
-                       animate('0.5s ease-in-out', 
+                       animate('1s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                        style({ transform: 'translateX(0%)' }),
-                       animate('0.5s ease-in-out', 
+                       animate('1s ease-in-out', 
                        style({ transform: 'translateX(-100%)' }))
                    ], { optional: true }),
                ])
@@ -67,12 +67,12 @@ import {
                group([
                    query(':enter', [
                        style({ transform: 'translateX(-100%)' }),
-                       animate('0.5s ease-in-out', 
+                       animate('1s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                         style({ transform: 'translateX(0%)' }),
-                        animate('0.5s ease-in-out', 
+                        animate('1s ease-in-out', 
                         style({ transform: 'translateX(100%)' }))
                    ], { optional: true }),
                ])
