dc2802e623e785dce07ecccf44a1aa5c9e8dc6f2
Denis <dmarkitanov@gmail.com>
2019-11-10 20:37:35 +0600

    added animation for thw board

--- src/app/shared/route-animation.ts
+++ src/app/shared/route-animation.ts
@@ -11,34 +11,34 @@ import {
     trigger('routeAnimations', [
          transition('Board => *', [
               query(':enter, :leave',
-                   style({ position: 'fixed', width: '100%' }), 
+                   style({ position: 'fixed', width: '100%', height: '100%', top: '0' }), 
                    { optional: true }),        
               group([
                    query(':enter',[
                        style({ transform: 'translateX(100%)' }),
-                       animate('1s ease-in-out', 
+                       animate('5s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                        style({ transform:   'translateX(0%)'}),
-                       animate('1s ease-in-out', 
-                       style({ transform: 'translateX(100%)' }))
+                       animate('5s ease-in-out', 
+                       style({ transform: 'translateX(-100%)' }))
                    ], { optional: true }),
               ])
          ]),
          transition('Home => *', [
               query(':enter, :leave', 
-                   style({ position: 'fixed',  width: '100%', background: 'white' }), 
+                   style({ position: 'fixed',  width: '100%' }), 
                    { optional: true }),
               group([
                    query(':enter', [
                        style({ transform: 'translateX(100%)' }), 
-                       animate('1s ease-in-out', 
+                       animate('5s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                        style({ transform: 'translateX(0%)' }),
-                       animate('1s ease-in-out', 
+                       animate('5s ease-in-out', 
                        style({ transform: 'translateX(-100%)' }))
                        ], { optional: true }),
                ])
@@ -50,29 +50,29 @@ import {
                group([
                    query(':enter', [
                        style({ transform: 'translateX(100%)' }),
-                       animate('1s ease-in-out', 
+                       animate('5s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                        style({ transform: 'translateX(0%)' }),
-                       animate('1s ease-in-out', 
+                       animate('5s ease-in-out', 
                        style({ transform: 'translateX(-100%)' }))
                    ], { optional: true }),
                ])
          ]),
          transition('Main => Board', [
                query(':enter, :leave', 
-                   style({ position: 'fixed', width: '100%' }), 
+                   style({ position: 'fixed', width: '100%', height: '100%', top: '0' }),
                    { optional: true }),
                group([
                    query(':enter', [
                        style({ transform: 'translateX(-100%)' }),
-                       animate('1s ease-in-out', 
+                       animate('5s ease-in-out', 
                        style({ transform: 'translateX(0%)' }))
                    ], { optional: true }),
                    query(':leave', [
                         style({ transform: 'translateX(0%)' }),
-                        animate('1s ease-in-out', 
+                        animate('5s ease-in-out', 
                         style({ transform: 'translateX(100%)' }))
                    ], { optional: true }),
                ])
