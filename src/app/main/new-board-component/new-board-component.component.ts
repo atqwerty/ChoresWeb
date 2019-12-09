704ef1d9f91c61b3d735b6e8a93f60e925c0d276
Denis <dmarkitanov@gmail.com>
2019-12-09 17:43:45 +0600

    im stupid :^)

--- src/app/main/new-board-component/new-board-component.component.ts
+++ src/app/main/new-board-component/new-board-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'
-import NewBoardDialogData from '../board-list/board-list.component'
+import { NewBoardDialogData } from '../board-list/board-list.component'
 
 @Component({
   selector: 'app-new-board-component',
